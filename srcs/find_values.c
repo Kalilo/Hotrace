9aa0a78921457678bdff2c24691117cb3ad4ad23
Kalilo <kalilohansmann@gmail.com>
2016-12-03 13:54:03 +0200

    Added find values function.

--- srcs/find_values.c
+++ srcs/find_values.c
@@ -12,6 +12,23 @@
 
 #include "../includes/hotrace.h"
 
+void	display_values(t_link *link, char *query, int k)
+{
+	if (ft_strcmp(link->key, query))
+		link = (k > 0) ? link->higher : (k < 0) ? link->lower : link->equal;
+	ft_putstr(query);
+	if (link == NULL || ft_strcmp(link->key, query))
+		ft_putendl(": Not Found.");
+	else
+	{
+		do {
+			ft_putstr(": ");
+			ft_putendl(link->value);
+			link = link->equal;
+		} while (link != NULL && !ft_strcmp(link->key, query));
+	}
+}
+
 void	find_values(void)
 {
 	t_query	*query;
@@ -23,15 +40,16 @@ void	find_values(void)
 	{
 		link = find_link_pos(g_index[find_index(query->query[0])], query->query);
 		k = strcmp(link->key, query->query);
-		link = (k > 0) ? link->higher : (k < 0) ? link->lower : link->equal;
-		ft_putstr(query->query);
+		//
+		display_values(link, query->query, k);
+		/*ft_putstr(query->query);
 		if (link == NULL)
 			ft_putendl(": Not Found");
 		else
 		{
 			ft_putstr(": ");
 			ft_putendl(link->value);
-		}
+		}*/
 		query = query->next;
 	}
 }
