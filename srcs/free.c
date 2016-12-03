e6730768bc3caf453513cf30dab38667a1a96c31
Kalilo <kalilohansmann@gmail.com>
2016-12-03 16:12:18 +0200

    Freed more

--- srcs/free.c
+++ srcs/free.c
@@ -14,18 +14,25 @@
 
 void	free_lindex(void)
 {
+	t_lindex	*tmp;
+
 	while (g_lindex != NULL)
 	{
 		destroy_link(g_lindex->ptr);
+		tmp = g_lindex;
 		g_lindex = g_lindex->next;
+		free(tmp);
 	}
 }
 
 void	free_query(void)
 {
+	t_query	*tmp;
 	while (g_query != NULL)
 	{
 		free(g_query->query);
+		tmp = g_query;
 		g_query = g_query->next;
+		free(tmp);
 	}
 }
